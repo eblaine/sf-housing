5294c0bf64e52cd239051b4cb625e4bfb14a7060
Ian <ianjones@stanford.edu>
2018-06-02 13:03:26 -0700

    updated data.js for 1990, 2000, 2010. 2018 is not yet accurate, just displaying what we had on the slides.

--- data.js
+++ data.js
@@ -17,4 +17,62 @@ var data = {
             'overburdened': 33
         }
     },
+
+    '2000': {
+        'low': {
+            'total': 23,
+            'overburdened': 65
+        },
+        'lowMiddle': {
+            'total': 12,
+            'overburdened': 58
+        },
+        'highMiddle': {
+            'total': 18,
+            'overburdened': 39
+        },
+        'high': {
+            'total': 47,
+            'overburdened': 9
+        }
+    },
+
+    '2010': {
+        'low': {
+            'total': 10,
+            'overburdened': 80
+        },
+        'lowMiddle': {
+            'total': 9,
+            'overburdened': 78
+        },
+        'highMiddle': {
+            'total': 15,
+            'overburdened': 53
+        },
+        'high': {
+            'total': 66,
+            'overburdened': 8
+        }
+    },
+
+
+    '2018': {
+        'low': {
+            'total': 6,
+            'overburdened': 83
+        },
+        'lowMiddle': {
+            'total': 7,
+            'overburdened': 71
+        },
+        'highMiddle': {
+            'total': 14,
+            'overburdened': 50
+        },
+        'high': {
+            'total': 73,
+            'overburdened': 7
+        }
+    },
 };
